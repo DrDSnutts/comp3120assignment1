f939fcea963c5a379765a8ff3028ff586cbbe88a
Dominic Wright <dominic@walkham.com>
2023-08-11 11:13:38 +1000

    testing frontend

--- src/App.js
+++ src/App.js
@@ -7,6 +7,32 @@ function App(){
   const [cart, setCart] = useState([])
   const [orders, setOrders] = useState([])
   const [user, setUser] = ({id:1})
+
+  console.log()
+
+  //fetch products
+  useEffect(() => {
+    fetch('/products')
+      .then(response => response.json())
+      .then(data => setProducts(data));
+  });
+
+ 
+
+  //fetch user orders
+  useEffect(() => {
+    fetch(`/orders?user_id=${user.id}`)
+      .then(response => response.json())
+      .then(data => setOrders(data));
+  }, [user.id]);
+
+  
+
+  return(
+    <div>
+      <h1>Test</h1>
+    </div>
+  )
 }
 
 export default App;
